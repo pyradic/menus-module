50a6ebf3613c277335c5f6c598b77117dbfd95c7
Robin Radic <rradic@hotmail.com>
2020-09-21 01:08:18 +0200

    upd

--- src/Link/LinkModel.php
+++ src/Link/LinkModel.php
@@ -4,8 +4,6 @@ use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
 use Anomaly\Streams\Platform\Entry\EntryCollection;
 use Anomaly\Streams\Platform\Model\Menus\MenusLinksEntryModel;
 use Illuminate\Support\Str;
-use League\Uri\Contracts\UriInterface;
-use League\Uri\Exceptions\SyntaxError;
 use League\Uri\Uri;
 use League\Uri\UriModifier;
 use Pyro\MenusModule\Link\Contract\LinkInterface;
@@ -21,70 +19,63 @@ use Pyro\MenusModule\Uri\UriValidator;
 /**
  * Pyro\MenusModule\Link\LinkModel
  *
- * @property int $id
- * @property int|null $sort_order
- * @property mixed $created_at
- * @property int|null $created_by_id
- * @property mixed $updated_at
- * @property int|null $updated_by_id
- * @property mixed $deleted_at
- * @property int $menu_id
- * @property \Pyro\MenusModule\Type\LinkTypeExtension $type
- * @property int $entry_id
- * @property string $entry_type
- * @property string $target
- * @property string|null $class
- * @property int|null $parent_id
- * @property string|null $icon
- * @property string|null $hash
- * @property string|null $querystring
- * @property \Pyro\ActivityLogModule\Activity\ActivityCollection|\Pyro\ActivityLogModule\Activity\ActivityModel[] $actions
- * @property int|null $actions_count
- * @property \Pyro\ActivityLogModule\Activity\ActivityCollection|\Pyro\ActivityLogModule\Activity\ActivityModel[] $activityLogs
- * @property int|null $activity_logs_count
- * @property \Anomaly\UsersModule\Role\RoleCollection|\Anomaly\UsersModule\Role\RoleModel[] $allowedRoles
- * @property int|null $allowed_roles_count
- * @property \Pyro\MenusModule\Link\LinkCollection|\Pyro\MenusModule\Link\LinkModel[] $children
- * @property int|null $children_count
- * @property \Anomaly\UsersModule\User\UserModel|null $createdBy
- * @property \Anomaly\UsersModule\User\UserModel|null $created_by
- * @property mixed|null $raw
- * @property \Pyro\MenusModule\Menu\MenuModel $menu
- * @property \Pyro\MenusModule\Link\LinkModel|null $parent
- * @property \Anomaly\UsersModule\User\UserModel|null $updatedBy
- * @property \Anomaly\UsersModule\User\UserModel|null $updated_by
+ * @property int                                                                                                  $id
+ * @property int|null                                                                                             $sort_order
+ * @property mixed                                                                                                $created_at
+ * @property int|null                                                                                             $created_by_id
+ * @property mixed                                                                                                $updated_at
+ * @property int|null                                                                                             $updated_by_id
+ * @property mixed                                                                                                $deleted_at
+ * @property int                                                                                                  $menu_id
+ * @property string                                                                                               $type
+ * @property int                                                                                                  $entry_id
+ * @property string                                                                                               $entry_type
+ * @property string                                                                                               $target
+ * @property string|null                                                                                          $class
+ * @property int|null                                                                                             $parent_id
+ * @property string|null                                                                                          $icon
+ * @property string|null                                                                                          $hash
+ * @property string|null                                                                                          $querystring
+ * @property \Anomaly\UsersModule\Role\RoleCollection|\Anomaly\UsersModule\Role\RoleModel[]                       $allowedRoles
+ * @property \Pyro\MenusModule\Link\LinkCollection|LinkModel[]                                                    $children
+ * @property \Anomaly\UsersModule\User\UserModel|null                                                             $createdBy
+ * @property \Anomaly\UsersModule\User\UserModel|null                                                             $created_by
+ * @property mixed|null                                                                                           $raw
+ * @property \Pyro\MenusModule\Menu\MenuModel                                                                     $menu
+ * @property LinkModel|null                                                                                       $parent
+ * @property \Anomaly\UsersModule\User\UserModel|null                                                             $updatedBy
+ * @property \Anomaly\UsersModule\User\UserModel|null                                                             $updated_by
  * @property \Anomaly\Streams\Platform\Version\VersionCollection|\Anomaly\Streams\Platform\Version\VersionModel[] $versions
- * @property int|null $versions_count
- * @property mixed $entry
- * @property \Anomaly\UsersModule\Role\RoleModel $allowed_roles
- * @method static \Pyro\MenusModule\Link\LinkCollection|static[] all($columns = ['*'])
- * @method static \Pyro\MenusModule\Link\LinkCollection|static[] get($columns = ['*'])
- * @method static \Pyro\MenusModule\Link\LinkModel make($attributes=[])
- * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|\Pyro\MenusModule\Link\LinkModel newModelQuery()
- * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|\Pyro\MenusModule\Link\LinkModel newQuery()
- * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|\Pyro\MenusModule\Link\LinkModel query()
- * @method static \Illuminate\Database\Eloquent\Builder|\Anomaly\Streams\Platform\Entry\EntryModel sorted($direction = 'asc')
- * @method static \Illuminate\Database\Eloquent\Builder|\Anomaly\Streams\Platform\Model\EloquentModel translated()
- * @method static \Illuminate\Database\Eloquent\Builder|\Anomaly\Streams\Platform\Model\EloquentModel translatedIn($locale)
+ * @property mixed                                                                                                $entry
+ * @property \Anomaly\UsersModule\Role\RoleModel                                                                  $allowed_roles
+ * @method static \Pyro\MenusModule\Link\LinkCollection|static[] all($columns = [ '*' ])
+ * @method static \Pyro\MenusModule\Link\LinkCollection|static[] get($columns = [ '*' ])
+ * @method static \Pyro\MenusModule\Link\LinkModel make($attributes = [])
+ * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|LinkModel newModelQuery()
+ * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|LinkModel newQuery()
+ * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|LinkModel query()
+ * @method static Builder|EntryModel sorted($direction = 'asc')
+ * @method static Builder|EloquentModel translated()
+ * @method static Builder|EloquentModel translatedIn($locale)
  * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereAllowedRoles($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereClass($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereCreatedById($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereClass($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereCreatedById($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereEntry($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereEntryId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereEntryType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereHash($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereIcon($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereMenuId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereParentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereQuerystring($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereSortOrder($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereTarget($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Link\LinkModel whereUpdatedById($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereEntryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereEntryType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereHash($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereIcon($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereMenuId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereQuerystring($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereSortOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereTarget($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkModel whereUpdatedById($value)
  * @mixin \Eloquent
  * @method \Pyro\MenusModule\Link\LinkPresenter getPresenter()
  * @method \Pyro\MenusModule\Link\LinkCollection newCollection()
@@ -180,11 +171,13 @@ class LinkModel extends MenusLinksEntryModel implements LinkInterface
             }
 
             $url = UriValidator::validate($uri) ? $uri->__toString() : $url;
-        } catch(\Throwable $e){
+        }
+        catch (\Throwable $e) {
 
         }
         return $url;
     }
+
     /**
      * Get the title.
      *
@@ -429,11 +422,11 @@ class LinkModel extends MenusLinksEntryModel implements LinkInterface
 
     public function setQuerystringAttribute($querystring)
     {
-        $this->attributes['querystring'] = Str::startsWith($querystring, '?') ? Str::replaceFirst('?','',$querystring) : $querystring;
+        $this->attributes[ 'querystring' ] = Str::startsWith($querystring, '?') ? Str::replaceFirst('?', '', $querystring) : $querystring;
     }
 
     public function setHashAttribute($hash)
     {
-        $this->attributes['hash'] = Str::startsWith($hash, '#') ? Str::replaceFirst('#', '',$hash) : $hash;
+        $this->attributes[ 'hash' ] = Str::startsWith($hash, '#') ? Str::replaceFirst('#', '', $hash) : $hash;
     }
 }
--- src/Menu/MenuModel.php
+++ src/Menu/MenuModel.php
@@ -11,57 +11,50 @@ use Pyro\MenusModule\Menu\Contract\MenuInterface;
  * \Pyro\MenusModule\Menu\MenuModel
  *
  */
+
 /**
  * Pyro\MenusModule\Menu\MenuModel
  *
- * @property int $id
- * @property int|null $sort_order
- * @property mixed $created_at
- * @property int|null $created_by_id
- * @property mixed $updated_at
- * @property int|null $updated_by_id
- * @property mixed $deleted_at
- * @property string $slug
- * @property \Pyro\ActivityLogModule\Activity\ActivityCollection|\Pyro\ActivityLogModule\Activity\ActivityModel[] $actions
- * @property int|null $actions_count
- * @property \Pyro\ActivityLogModule\Activity\ActivityCollection|\Pyro\ActivityLogModule\Activity\ActivityModel[] $activityLogs
- * @property int|null $activity_logs_count
- * @property \Anomaly\UsersModule\Role\RoleCollection|\Anomaly\UsersModule\Role\RoleModel[] $allowedRoles
- * @property int|null $allowed_roles_count
- * @property \Anomaly\UsersModule\User\UserModel|null $createdBy
- * @property \Anomaly\UsersModule\User\UserModel|null $created_by
- * @property mixed|null $raw
- * @property \Pyro\MenusModule\Link\LinkCollection|\Pyro\MenusModule\Link\LinkModel[] $links
- * @property int|null $links_count
+ * @property int                                                                                                                         $id
+ * @property int|null                                                                                                                    $sort_order
+ * @property mixed                                                                                                                       $created_at
+ * @property int|null                                                                                                                    $created_by_id
+ * @property mixed                                                                                                                       $updated_at
+ * @property int|null                                                                                                                    $updated_by_id
+ * @property mixed                                                                                                                       $deleted_at
+ * @property string                                                                                                                      $slug
+ * @property \Anomaly\UsersModule\Role\RoleCollection|\Anomaly\UsersModule\Role\RoleModel[]                                              $allowedRoles
+ * @property \Anomaly\UsersModule\User\UserModel|null                                                                                    $createdBy
+ * @property \Anomaly\UsersModule\User\UserModel|null                                                                                    $created_by
+ * @property mixed|null                                                                                                                  $raw
+ * @property LinkCollection|LinkModel[]                                                                                                  $links
  * @property \Anomaly\Streams\Platform\Model\EloquentCollection|\Anomaly\Streams\Platform\Model\Menus\MenusMenusEntryTranslationsModel[] $translations
- * @property int|null $translations_count
- * @property \Anomaly\UsersModule\User\UserModel|null $updatedBy
- * @property \Anomaly\UsersModule\User\UserModel|null $updated_by
- * @property \Anomaly\Streams\Platform\Version\VersionCollection|\Anomaly\Streams\Platform\Version\VersionModel[] $versions
- * @property int|null $versions_count
- * @property string $name
- * @property string $description
- * @property \Anomaly\UsersModule\Role\RoleModel $allowed_roles
- * @method static \Anomaly\Streams\Platform\Entry\EntryCollection|static[] all($columns = ['*'])
- * @method static \Anomaly\Streams\Platform\Entry\EntryCollection|static[] get($columns = ['*'])
- * @method static \Pyro\MenusModule\Menu\MenuModel make($attributes=[])
- * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|\Pyro\MenusModule\Menu\MenuModel newModelQuery()
- * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|\Pyro\MenusModule\Menu\MenuModel newQuery()
- * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|\Pyro\MenusModule\Menu\MenuModel query()
- * @method static \Illuminate\Database\Eloquent\Builder|\Anomaly\Streams\Platform\Entry\EntryModel sorted($direction = 'asc')
- * @method static \Illuminate\Database\Eloquent\Builder|\Anomaly\Streams\Platform\Model\EloquentModel translated()
- * @method static \Illuminate\Database\Eloquent\Builder|\Anomaly\Streams\Platform\Model\EloquentModel translatedIn($locale)
+ * @property \Anomaly\UsersModule\User\UserModel|null                                                                                    $updatedBy
+ * @property \Anomaly\UsersModule\User\UserModel|null                                                                                    $updated_by
+ * @property \Anomaly\Streams\Platform\Version\VersionCollection|\Anomaly\Streams\Platform\Version\VersionModel[]                        $versions
+ * @property string                                                                                                                      $name
+ * @property string                                                                                                                      $description
+ * @property \Anomaly\UsersModule\Role\RoleModel                                                                                         $allowed_roles
+ * @method static \Anomaly\Streams\Platform\Entry\EntryCollection|static[] all($columns = [ '*' ])
+ * @method static \Anomaly\Streams\Platform\Entry\EntryCollection|static[] get($columns = [ '*' ])
+ * @method static \Pyro\MenusModule\Menu\MenuModel make($attributes = [])
+ * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|MenuModel newModelQuery()
+ * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|MenuModel newQuery()
+ * @method static \Anomaly\Streams\Platform\Entry\EntryQueryBuilder|MenuModel query()
+ * @method static Builder|EntryModel sorted($direction = 'asc')
+ * @method static Builder|EloquentModel translated()
+ * @method static Builder|EloquentModel translatedIn($locale)
  * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereAllowedRoles($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereCreatedById($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MenuModel whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MenuModel whereCreatedById($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MenuModel whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MenuModel whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereSortOrder($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Pyro\MenusModule\Menu\MenuModel whereUpdatedById($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MenuModel whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MenuModel whereSortOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MenuModel whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MenuModel whereUpdatedById($value)
  * @mixin \Eloquent
  * @method \Anomaly\Streams\Platform\Entry\EntryPresenter getPresenter()
  * @method \Anomaly\Streams\Platform\Entry\EntryCollection newCollection()
