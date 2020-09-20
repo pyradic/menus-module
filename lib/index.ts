38b7699205411e0850a6471aaa8216c4ed9fc291
Robin Radic <rradic@hotmail.com>
2020-09-20 15:31:24 +0200

    upd

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,12 @@
-import './menus.scss'
-import './vendor/jquery.blockUI'
+import './menus.scss';
+import './vendor/jquery.blockUI';
 import { MenusModuleServiceProvider } from './MenusModuleServiceProvider';
-import MenusVuePlugin from './MenusVuePlugin';
+import MenusVuePlugin                 from './MenusVuePlugin';
 
 export {
     MenusModuleServiceProvider,
-    MenusVuePlugin
-}
+    MenusVuePlugin,
+};
 
 
-
+export * from './interfaces';
